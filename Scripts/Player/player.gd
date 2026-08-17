36c7f67c530dae5e039bcc16e6454a9255a60cf1
vicstroie <158312626+vicstroie@users.noreply.github.com>
2026-08-17 17:37:27 -0400

    done with inventory for now

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -42,7 +42,7 @@ var inventory_ui
 
 @export_category("Player Data Info")
 @export var health : float
-@export var inventory_size : int = 8
+@export var inventory_size : int = 8 #DO NOT CHANGE
 var status_dictionary
 var inventory_dictionary 
 var database
